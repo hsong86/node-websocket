a43fc2ea995798b7f283c55f9bdedf71c9cc8a85
hsong86 <howtotrade@hotmail.com>
2013-02-02 23:11:12 -0500

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,10 +1,21 @@
+var express = require(‘express’); 
+var app = express.createServer(); 
+var port = process.env.PORT || 3000;
+
+app.get(‘/’, function(request, response) { 
+  response.send(‘Hello Engine Yard Cloud!’);
+});
+
+app.listen(port);
+
+/*
 //HTTP part
 var server = require('http').createServer(function(req, res){
   res.writeHead(200,{ 'Content-Type': 'text/html' });
   res.end('<h1>Try the same on socket!</h1>');
 });
 server.listen(80);
-/*
+
 //some servers may require port 8080
 var server = require('ws').createServer({port:8080});
 server.on('connection', function(conn) {
